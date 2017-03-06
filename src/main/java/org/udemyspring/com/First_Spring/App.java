9d32762ce209a8464f4ca4b61fe5e8724a1a59d3
saketh92 <saisakethnandagiri@gmail.com>
2017-03-06 13:56:11 -0500

    Second Commit

---
+++ src/main/java/org/udemyspring/com/First_Spring/App.java
@@ -0,0 +1,13 @@
+package org.udemyspring.com.First_Spring;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
